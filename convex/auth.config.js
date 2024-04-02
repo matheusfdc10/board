91a245e0a326ceade3c95520a97a2b9ece3e90d3
matheusfdc10 <64614285+matheusfdc10@users.noreply.github.com>
2024-04-02 17:05:47 -0300

    f

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://pleasing-baboon-76.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
---
+++ src/components/hint.tsx
@@ -0,0 +1,45 @@
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger
+} from "@/components/ui/tooltip"
+
+export interface HintProps {
+    label: string;
+    children: React.ReactNode;
+    side?: "top" | "bottom" | "left" | "right";
+    align?: "start" | "center" | "end";
+    sideOffset?: number;
+    alingnOffset?: number;
+}
+
+export const Hint: React.FC<HintProps> = ({
+    label,
+    children,
+    side,
+    align,
+    alingnOffset,
+    sideOffset
+}) => {
+    return (
+        <TooltipProvider>
+            <Tooltip delayDuration={100}>
+                <TooltipTrigger asChild>
+                    {children}
+                </TooltipTrigger>
+                <TooltipContent
+                    className="text-white bg-black border-black"
+                    side={side}
+                    align={align}
+                    sideOffset={sideOffset}
+                    alignOffset={alingnOffset}
+                >
+                    <p className="font-semibold capitalize">
+                        {label}
+                    </p>
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+    )
+}
